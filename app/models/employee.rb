305c3aa6f99669184e17509c371b22f21939c39f
Hunter Posey <ihposey@crimson.ua.edu>
2020-12-21 09:44:00 -0800

    which bear is best

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,13 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :title, presence: true
+    validates :office, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :title, uniqueness: true
+    validates :alias, uniqueness: true
+
+    def f_and_l
+        "#{self.first_name} #{self.last_name}"
+    end
 end
